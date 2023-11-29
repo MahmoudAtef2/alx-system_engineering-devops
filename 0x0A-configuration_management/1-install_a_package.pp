95f17a0445ae717ac8038dc0ec852af5ccdd2f37
MahmoudAtef2 <matef3431@gmail.com>
2023-11-29 09:49:18 -0800

    all tasks

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,11 @@
+#!/usr/bin/pup
+# Puppet, install flask from pip3.
+package {'flask':
+ensure   => '2.1.0',
+provider => 'pip3'
+}
+# install Werkzeug 2.1.1
+package {'Werkzeug':
+ensure   => '2.1.1',
+provider => 'pip3'
+}
