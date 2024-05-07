45e5afb9b6251096a30d8d37a276d7e35703ffd8
MahmoudAtef2 <matef3431@gmail.com>
2024-05-07 19:29:36 +0300

    done

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""define module total subs"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+        """number of subscribers"""
+            if subreddit is None or type(subreddit) is not str:
+                        return (0)
+                        url = f'http://www.reddit.com/r/{subreddit}/about.json'
+                            headers = {'User-Agent': 'hamzaApp/1.0 by hamza-25'}
+                                response = requests.get(url, headers=headers)
+                                    response_json = response.json()
+                                        subs = response_json.get("data", {}).get("subscribers", 0)
+                                            return int(subs)
---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""define module print 10 title"""
+import requests
+
+
+def top_ten(subreddit):
+        """print title"""
+            if subreddit is None or type(subreddit) is not str:
+                        return (0)
+                        url = f'https://www.reddit.com/r/{subreddit}/hot/.json'
+                            headers = {'User-Agent': 'hamzaApp/1.0 by hamza-25'}
+                                params = {'limit': 10}
+                                    response = requests.get(url, headers=headers,
+                                                                        params=params, allow_redirects=False)
+                                        if response.status_code == 404:
+                                                    print("None")
+                                                            return
+                                                            data = response.json().get('data')
+                                                                for title in data.get('children'):
+                                                                            print(title.get('data').get('title'))
