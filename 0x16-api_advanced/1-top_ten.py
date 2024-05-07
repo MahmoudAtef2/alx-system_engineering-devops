40ae13cd7724cc64aa311393bddb0237d1086698
MahmoudAtef2 <matef3431@gmail.com>
2024-05-07 19:31:23 +0300

    done

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -4,17 +4,17 @@ import requests
 
 
 def top_ten(subreddit):
-        """print title"""
-            if subreddit is None or type(subreddit) is not str:
-                        return (0)
-                        url = f'https://www.reddit.com/r/{subreddit}/hot/.json'
-                            headers = {'User-Agent': 'hamzaApp/1.0 by hamza-25'}
-                                params = {'limit': 10}
-                                    response = requests.get(url, headers=headers,
-                                                                        params=params, allow_redirects=False)
-                                        if response.status_code == 404:
-                                                    print("None")
-                                                            return
-                                                            data = response.json().get('data')
-                                                                for title in data.get('children'):
-                                                                            print(title.get('data').get('title'))
+    """print title"""
+    if subreddit is None or type(subreddit) is not str:
+        return (0)
+    url = f'https://www.reddit.com/r/{subreddit}/hot/.json'
+    headers = {'User-Agent': 'hamzaApp/1.0 by hamza-25'}
+    params = {'limit': 10}
+    response = requests.get(url, headers=headers,
+                            params=params, allow_redirects=False)
+    if response.status_code == 404:
+        print("None")
+        return
+    data = response.json().get('data')
+    for title in data.get('children'):
+        print(title.get('data').get('title'))
